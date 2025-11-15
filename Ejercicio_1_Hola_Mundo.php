7c086cd4d0db0363d3afc2c0d2ba095a9e058a85
joysg1 <joy.nelaton@utp.ac.pa>
2025-11-14 22:16:58 -0500

    Create Ejercicio_1_Hola_Mundo.php

---
+++ Ejercicio_1_Hola_Mundo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hola Mundo PHP</title>
+</head>
+<body>
+    <h1><?php echo "¡Hola Mundo!"; ?></h1>
+    <p>Fecha actual: <?php echo date('Y-m-d H:i:s'); ?></p>
+    <p>Estás ejecutando PHP versión: <?php echo phpversion(); ?></p>
+</body>
+</html>
